257bf6fef86d1d87f8d59643d0f14af694ae8312
jerpelo <jasmine.erpelo@gmail.com>
2022-02-20 20:32:26 -0500

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <a href="/">Open-source code</a> by Jasmine Oriola
+        </footer>
+      </div>
     </div>
   );
 }
