73e0d30190ac6778d3e7ef6e945e884ed9513082
jerpelo <jasmine.erpelo@gmail.com>
2022-02-20 20:52:10 -0500

    integrated dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}...`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   return (
     <div className="Dictionary">
