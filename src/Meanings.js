3bcddab95b9e028ac7712cafdee5bd1efb55db4e
jerpelo <jasmine.erpelo@gmail.com>
2022-02-21 12:27:21 -0500

    added synonyms

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,16 +1,21 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meanings(props) {
   return (
     <div className="Meanings">
       <h3>{props.meanings.partOfSpeech}</h3>
-      {props.meanings.definitions.map(function (definition, index) {
+      {props.meanings.definitions.map(function (definitions, index) {
         return (
           <div key={index}>
             <p>
-              {definition.definition}
+              <strong>Definition: </strong>
+              {definitions.definition}
               <br />
-              <em>{definition.example}</em>
+              <strong>Example: </strong>
+              <em>{definitions.example}</em>
+              <br />
+              <Synonyms synonyms={definitions.synonyms} />
             </p>
           </div>
         );
